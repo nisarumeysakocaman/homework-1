0dba747bd376016065a4f42d4743da67f35c7f38
nisarumeysakocaman <147979364+nisarumeysakocaman@users.noreply.github.com>
2023-10-24 15:00:18 +0300

    homework 1

---
+++ homework.cpp
@@ -0,0 +1,30 @@
+Start
+int pass,class,time,price,total price;
+pass==p;
+class==c;
+time==t
+economy class==ec;
+first class==fc;
+price==pr;
+total price==tpr;
+if <c==ec>
+  if <t==morning>;
+    print"pr==100*p";
+   else if <t==afternoon>
+    print"pr==90*p";
+   else 
+    print"pr==70*p";
+else if <c==fc> 
+   if <t==morning>;
+   print"pr==150*p";
+   else if <t==afternoon>
+   print"pr==140*p";
+   else
+   print"pr==120*p";
+if <p>2>
+    print"tpr==pr*0.85";
+    else
+    print"pr";
+else 
+print"false choice";
+End
